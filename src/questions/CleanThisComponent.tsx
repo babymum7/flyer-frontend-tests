2f3390112b9ff10e0fab4c4188426bd37b14d864
babymum7 <nbtrong2312@gmail.com>
2022-05-16 04:23:10 +0700

    update

--- src/questions/CleanThisComponent.tsx
+++ src/questions/CleanThisComponent.tsx
@@ -144,7 +144,7 @@ export default function Example() {
       <div className="bg-white">
         {/* Mobile menu */}
         <Transition.Root show={open} as={Fragment}>
-          <Dialog as="div" className="fixed inset-0 flex z-40 lg:hidden" onClose={setOpen}>
+          <Dialog as="div" className="fixed inset-0 z-40 flex lg:hidden" onClose={setOpen}>
             <Transition.Child
               as={Fragment}
               enter="transition-opacity ease-linear duration-300"
@@ -166,22 +166,22 @@ export default function Example() {
               leaveFrom="translate-x-0"
               leaveTo="-translate-x-full"
             >
-              <div className="relative max-w-xs w-full bg-white shadow-xl pb-12 flex flex-col overflow-y-auto">
-                <div className="px-4 pt-5 pb-2 flex">
+              <div className="relative flex flex-col w-full max-w-xs pb-12 overflow-y-auto bg-white shadow-xl">
+                <div className="flex px-4 pt-5 pb-2">
                   <button
                     type="button"
-                    className="-m-2 p-2 rounded-md inline-flex items-center justify-center text-gray-400"
+                    className="inline-flex items-center justify-center p-2 -m-2 text-gray-400 rounded-md"
                     onClick={() => setOpen(false)}
                   >
                     <span className="sr-only">Close menu</span>
-                    <XIcon className="h-6 w-6" aria-hidden="true" />
+                    <XIcon className="w-6 h-6" aria-hidden="true" />
                   </button>
                 </div>
 
                 {/* Links */}
                 <Tab.Group as="div" className="mt-2">
                   <div className="border-b border-gray-200">
-                    <Tab.List className="-mb-px flex px-4 space-x-8">
+                    <Tab.List className="flex px-4 -mb-px space-x-8">
                       {navigation.categories.map((category) => (
                         <Tab
                           key={category.name}
@@ -201,19 +201,19 @@ export default function Example() {
                   </div>
                   <Tab.Panels as={Fragment}>
                     {navigation.categories.map((category) => (
-                      <Tab.Panel key={category.name} className="pt-10 pb-8 px-4 space-y-10">
+                      <Tab.Panel key={category.name} className="px-4 pt-10 pb-8 space-y-10">
                         <div className="grid grid-cols-2 gap-x-4">
                           {category.featured.map((item) => (
-                            <div key={item.name} className="group relative text-sm">
-                              <div className="aspect-w-1 aspect-h-1 rounded-lg bg-gray-100 overflow-hidden group-hover:opacity-75">
+                            <div key={item.name} className="relative text-sm group">
+                              <div className="overflow-hidden bg-gray-100 rounded-lg aspect-w-1 aspect-h-1 group-hover:opacity-75">
                                 <img
                                   src={item.imageSrc}
                                   alt={item.imageAlt}
-                                  className="object-center object-cover"
+                                  className="object-cover object-center"
                                 />
                               </div>
-                              <a href={item.href} className="mt-6 block font-medium text-gray-900">
-                                <span className="absolute z-10 inset-0" aria-hidden="true" />
+                              <a href={item.href} className="block mt-6 font-medium text-gray-900">
+                                <span className="absolute inset-0 z-10" aria-hidden="true" />
                                 {item.name}
                               </a>
                               <p aria-hidden="true" className="mt-1">
@@ -232,11 +232,11 @@ export default function Example() {
                             </p>
                             <ul
                               aria-labelledby={`${category.id}-${section.id}-heading-mobile`}
-                              className="mt-6 flex flex-col space-y-6"
+                              className="flex flex-col mt-6 space-y-6"
                             >
                               {section.items.map((item) => (
                                 <li key={item.name} className="flow-root">
-                                  <a href={item.href} className="-m-2 p-2 block text-gray-500">
+                                  <a href={item.href} className="block p-2 -m-2 text-gray-500">
                                     {item.name}
                                   </a>
                                 </li>
@@ -249,33 +249,33 @@ export default function Example() {
                   </Tab.Panels>
                 </Tab.Group>
 
-                <div className="border-t border-gray-200 py-6 px-4 space-y-6">
+                <div className="px-4 py-6 space-y-6 border-t border-gray-200">
                   {navigation.pages.map((page) => (
                     <div key={page.name} className="flow-root">
-                      <a href={page.href} className="-m-2 p-2 block font-medium text-gray-900">
+                      <a href={page.href} className="block p-2 -m-2 font-medium text-gray-900">
                         {page.name}
                       </a>
                     </div>
                   ))}
                 </div>
 
-                <div className="border-t border-gray-200 py-6 px-4 space-y-6">
+                <div className="px-4 py-6 space-y-6 border-t border-gray-200">
                   <div className="flow-root">
-                    <span className="-m-2 p-2 block font-medium text-gray-900">Sign in</span>
+                    <span className="block p-2 -m-2 font-medium text-gray-900">Sign in</span>
                   </div>
                   <div className="flow-root">
-                    <span className="-m-2 p-2 block font-medium text-gray-900">Create account</span>
+                    <span className="block p-2 -m-2 font-medium text-gray-900">Create account</span>
                   </div>
                 </div>
 
-                <div className="border-t border-gray-200 py-6 px-4">
-                  <span className="-m-2 p-2 flex items-center">
+                <div className="px-4 py-6 border-t border-gray-200">
+                  <span className="flex items-center p-2 -m-2">
                     <img
                       src="https://tailwindui.com/img/flags/flag-canada.svg"
                       alt=""
-                      className="w-5 h-auto block flex-shrink-0"
+                      className="flex-shrink-0 block w-5 h-auto"
                     />
-                    <span className="ml-3 block text-base font-medium text-gray-900">CAD</span>
+                    <span className="block ml-3 text-base font-medium text-gray-900">CAD</span>
                     <span className="sr-only">, change currency</span>
                   </span>
                 </div>
@@ -285,28 +285,28 @@ export default function Example() {
         </Transition.Root>
 
         <header className="relative bg-white">
-          <p className="bg-indigo-600 h-10 flex items-center justify-center text-sm font-medium text-white px-4 sm:px-6 lg:px-8">
+          <p className="flex items-center justify-center h-10 px-4 text-sm font-medium text-white bg-indigo-600 sm:px-6 lg:px-8">
             Get free delivery on orders over $100
           </p>
 
-          <nav aria-label="Top" className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+          <nav aria-label="Top" className="px-4 mx-auto max-w-7xl sm:px-6 lg:px-8">
             <div className="border-b border-gray-200">
-              <div className="h-16 flex items-center">
+              <div className="flex items-center h-16">
                 <button
                   type="button"
-                  className="bg-white p-2 rounded-md text-gray-400 lg:hidden"
+                  className="p-2 text-gray-400 bg-white rounded-md lg:hidden"
                   onClick={() => setOpen(true)}
                 >
                   <span className="sr-only">Open menu</span>
-                  <MenuIcon className="h-6 w-6" aria-hidden="true" />
+                  <MenuIcon className="w-6 h-6" aria-hidden="true" />
                 </button>
 
                 {/* Logo */}
-                <div className="ml-4 flex lg:ml-0">
+                <div className="flex ml-4 lg:ml-0">
                   <span>
                     <span className="sr-only">Workflow</span>
                     <img
-                      className="h-8 w-auto"
+                      className="w-auto h-8"
                       src="https://tailwindui.com/img/logos/workflow-mark.svg?color=indigo&shade=600"
                       alt=""
                     />
@@ -315,7 +315,7 @@ export default function Example() {
 
                 {/* Flyout menus */}
                 <Popover.Group className="hidden lg:ml-8 lg:block lg:self-stretch">
-                  <div className="h-full flex space-x-8">
+                  <div className="flex h-full space-x-8">
                     {navigation.categories.map((category) => (
                       <Popover key={category.name} className="flex">
                         {({ open }) => (
@@ -342,35 +342,35 @@ export default function Example() {
                               leaveFrom="opacity-100"
                               leaveTo="opacity-0"
                             >
-                              <Popover.Panel className="absolute top-full inset-x-0 text-sm text-gray-500">
+                              <Popover.Panel className="absolute inset-x-0 text-sm text-gray-500 top-full">
                                 {/* Presentational element used to render the bottom shadow, if we put the shadow on the actual panel it pokes out the top, so we use this shorter element to hide the top of the shadow */}
                                 <div
-                                  className="absolute inset-0 top-1/2 bg-white shadow"
+                                  className="absolute inset-0 bg-white shadow top-1/2"
                                   aria-hidden="true"
                                 />
 
                                 <div className="relative bg-white">
-                                  <div className="max-w-7xl mx-auto px-8">
-                                    <div className="grid grid-cols-2 gap-y-10 gap-x-8 py-16">
-                                      <div className="col-start-2 grid grid-cols-2 gap-x-8">
+                                  <div className="px-8 mx-auto max-w-7xl">
+                                    <div className="grid grid-cols-2 py-16 gap-y-10 gap-x-8">
+                                      <div className="grid grid-cols-2 col-start-2 gap-x-8">
                                         {category.featured.map((item) => (
                                           <div
                                             key={item.name}
-                                            className="group relative text-base sm:text-sm"
+                                            className="relative text-base group sm:text-sm"
                                           >
-                                            <div className="aspect-w-1 aspect-h-1 rounded-lg bg-gray-100 overflow-hidden group-hover:opacity-75">
+                                            <div className="overflow-hidden bg-gray-100 rounded-lg aspect-w-1 aspect-h-1 group-hover:opacity-75">
                                               <img
                                                 src={item.imageSrc}
                                                 alt={item.imageAlt}
-                                                className="object-center object-cover"
+                                                className="object-cover object-center"
                                               />
                                             </div>
                                             <a
                                               href={item.href}
-                                              className="mt-6 block font-medium text-gray-900"
+                                              className="block mt-6 font-medium text-gray-900"
                                             >
                                               <span
-                                                className="absolute z-10 inset-0"
+                                                className="absolute inset-0 z-10"
                                                 aria-hidden="true"
                                               />
                                               {item.name}
@@ -381,7 +381,7 @@ export default function Example() {
                                           </div>
                                         ))}
                                       </div>
-                                      <div className="row-start-1 grid grid-cols-3 gap-y-10 gap-x-8 text-sm">
+                                      <div className="grid grid-cols-3 row-start-1 text-sm gap-y-10 gap-x-8">
                                         {category.sections.map((section) => (
                                           <div key={section.name}>
                                             <p
@@ -430,25 +430,25 @@ export default function Example() {
                   </div>
                 </Popover.Group>
 
-                <div className="ml-auto flex items-center">
+                <div className="flex items-center ml-auto">
                   <div className="hidden lg:flex lg:flex-1 lg:items-center lg:justify-end lg:space-x-6">
                     <span className="text-sm font-medium text-gray-700 hover:text-gray-800">
                       Sign in
                     </span>
-                    <span className="h-6 w-px bg-gray-200" aria-hidden="true" />
+                    <span className="w-px h-6 bg-gray-200" aria-hidden="true" />
                     <span className="text-sm font-medium text-gray-700 hover:text-gray-800">
                       Create account
                     </span>
                   </div>
 
                   <div className="hidden lg:ml-8 lg:flex">
-                    <span className="text-gray-700 hover:text-gray-800 flex items-center">
+                    <span className="flex items-center text-gray-700 hover:text-gray-800">
                       <img
                         src="https://tailwindui.com/img/flags/flag-canada.svg"
                         alt=""
-                        className="w-5 h-auto block flex-shrink-0"
+                        className="flex-shrink-0 block w-5 h-auto"
                       />
-                      <span className="ml-3 block text-sm font-medium">CAD</span>
+                      <span className="block ml-3 text-sm font-medium">CAD</span>
                       <span className="sr-only">, change currency</span>
                     </span>
                   </div>
@@ -462,10 +462,10 @@ export default function Example() {
                   </div>
 
                   {/* Cart */}
-                  <div className="ml-4 flow-root lg:ml-6">
-                    <span className="group -m-2 p-2 flex items-center">
+                  <div className="flow-root ml-4 lg:ml-6">
+                    <span className="flex items-center p-2 -m-2 group">
                       <ShoppingBagIcon
-                        className="flex-shrink-0 h-6 w-6 text-gray-400 group-hover:text-gray-500"
+                        className="flex-shrink-0 w-6 h-6 text-gray-400 group-hover:text-gray-500"
                         aria-hidden="true"
                       />
                       <span className="ml-2 text-sm font-medium text-gray-700 group-hover:text-gray-800">
